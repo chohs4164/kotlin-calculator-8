d0c17805963da55af052c70a6fabe6b1586ed72d
chohs4164 <josh100as@gmail.com>
2025-10-18 01:22:41 +0900

    feat(main): 프로그램 초기 구조 및 입력 로직 구현
    덧셈 계산기 프로그램의 시작을 위해 main 함수 내에
    초기 변수(sum,v1,v2,seperator)를 선언하였고
    사용자에게 입력을 요청하는 안내 문구를 추가하였습니다.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,4 +2,15 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
+    var sum = 0
+    var v1: Int
+    var v2: Int
+    var separator = " "
+
+    // 사용자가 입력을 하지 않거나 엔터만 입력하면 null이 반환될 수 있다는 것을 사용자에게 알림
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input: String? = readLine()
+
 }
+
+
